9f47abb6b48b35cc5b8eb63d8ff314c11ecd01d4
ag <ag@PROELE64>
2024-03-15 13:50:33 +0100

    Restructuring

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-class StarSystem {
+class Waypoint {
     constructor(name, CoordinateX, CoordinateY, CoordinateZ) {
       this.name = name;
       this.X = CoordinateX;
@@ -7,37 +7,50 @@ class StarSystem {
     }
 }
 
-//var UserInput 
-
 //Temporary hardcoded systems
-const HSystem01 = new StarSystem("Sol", 0, 0, 0);
-const HSystem02 = new StarSystem("Alpha Centauri", 3.03125, -0.09375, 3.15625);
-const HSystem03 = new StarSystem("Epsilon Indi", 3.125, -8.875, 7.125);
-const HSystem04 = new StarSystem("Barnard's star", -3.03125, 1.375, 4.9375);
-const HSystem05 = new StarSystem("Wolf 359", 3.875, 6.46875, -1.90625);
-const HSystem06 = new StarSystem("Epsilon Eridani", 1.9375, -7.75, -6.84375);
-const HSystem07 = new StarSystem("61 Cygni", -11.21875, -1.1875, 1.40625);
-const HSystem08 = new StarSystem("Groombridge 34", -9.90625, -3.6875, -5.09375);
-const HSystem09 = new StarSystem("Tau Ceti", -0.375, -11.40625, -3.5);
-const HSystem10 = new StarSystem("Luyten's Star",  6.5625, 2.34375, -10.25);
+const HWaypoint01 = new Waypoint("Sol", 0, 0, 0);
+const HWaypoint02 = new Waypoint("Alpha Centauri", 3.03125, -0.09375, 3.15625);
+const HWaypoint03 = new Waypoint("Epsilon Indi", 3.125, -8.875, 7.125);
+const HWaypoint04 = new Waypoint("Barnard's star", -3.03125, 1.375, 4.9375);
+const HWaypoint05 = new Waypoint("Wolf 359", 3.875, 6.46875, -1.90625);
+const HWaypoint06 = new Waypoint("Epsilon Eridani", 1.9375, -7.75, -6.84375);
+const HWaypoint07 = new Waypoint("61 Cygni", -11.21875, -1.1875, 1.40625);
+const HWaypoint08 = new Waypoint("Groombridge 34", -9.90625, -3.6875, -5.09375);
+const HWaypoint09 = new Waypoint("Tau Ceti", -0.375, -11.40625, -3.5);
+const HWaypoint10 = new Waypoint("Luyten's Star",  6.5625, 2.34375, -10.25);
+//Temporary hardcoded systems
 
-//Function QueryDatabaseforData
-    //Send UserInput to Database
-    //Requesting coordinates
-    //Place System Data into new const StarSystem
+//var UserInput = input
 
-//Distance Calculation
-function CalculateDistance(StarSystem01, StarSystem02) {
-    var a = StarSystem02.X - StarSystem01.X;
-    var b = StarSystem02.Y - StarSystem01.Y;
-    var c = StarSystem02.Z - StarSystem01.Z;
+function QueryDatabaseForData(UserInput)
+    //Send UserInput to Database via API
+    //Request name and coordinates
+    //Place System Data into new const Waypoint
+    //GET https://www.edsm.net/api-v1/system
+    //GET https://www.edsm.net/api-v1/system?systemName=Sol&showCoordinates=1
+    //  systemName
+    //  showCoordinates
+
+function TableAddWaypoint(){
+
+}
+
+function TableRemoveWaypoint(){
+
+}
+
+function TableSetWaypointToStart(){
+
+}
+
+function CalculateDistance(Waypoint01, Waypoint02) {
+    var a = Waypoint02.X - Waypoint01.X;
+    var b = Waypoint02.Y - Waypoint01.Y;
+    var c = Waypoint02.Z - Waypoint01.Z;
     var result = Math.sqrt(a * a + b * b + c * c);
     return (result.toFixed(2));
 }
 
-//Function addStarSystemObjectToListVisibleToUser
+function OptimizeRoute(TableContent, TableStart){
 
-//Function SetUserChosenSystemAsStartingPoint
-
-//Function OptimizeRoute
-    //
+}
