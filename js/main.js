5e5beeaacec2cc68ca0e44854e8b3ab0cd67d269
ag <ag@PROELE64>
2024-03-19 09:16:24 +0100

    additional js functionality

--- js/main.js
+++ js/main.js
@@ -1,56 +1,91 @@
 class Waypoint {
-    constructor(name, CoordinateX, CoordinateY, CoordinateZ) {
-      this.name = name;
-      this.X = CoordinateX;
-      this.Y = CoordinateY;
-      this.Z = CoordinateZ;
+    constructor(name, x, y, z) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 }
 
-//Temporary hardcoded systems
-const HWaypoint01 = new Waypoint("Sol", 0, 0, 0);
-const HWaypoint02 = new Waypoint("Alpha Centauri", 3.03125, -0.09375, 3.15625);
-const HWaypoint03 = new Waypoint("Epsilon Indi", 3.125, -8.875, 7.125);
-const HWaypoint04 = new Waypoint("Barnard's star", -3.03125, 1.375, 4.9375);
-const HWaypoint05 = new Waypoint("Wolf 359", 3.875, 6.46875, -1.90625);
-const HWaypoint06 = new Waypoint("Epsilon Eridani", 1.9375, -7.75, -6.84375);
-const HWaypoint07 = new Waypoint("61 Cygni", -11.21875, -1.1875, 1.40625);
-const HWaypoint08 = new Waypoint("Groombridge 34", -9.90625, -3.6875, -5.09375);
-const HWaypoint09 = new Waypoint("Tau Ceti", -0.375, -11.40625, -3.5);
-const HWaypoint10 = new Waypoint("Luyten's Star",  6.5625, 2.34375, -10.25);
-//Temporary hardcoded systems
+// replace with get request
+const waypoints = [
+    new Waypoint("Sol", 0, 0, 0),
+    new Waypoint("Alpha Centauri", 3.03125, -0.09375, 3.15625),
+    new Waypoint("Epsilon Indi", 3.125, -8.875, 7.125),
+    new Waypoint("Barnard's star", -3.03125, 1.375, 4.9375),
+    new Waypoint("Wolf 359", 3.875, 6.46875, -1.90625),
+    new Waypoint("Epsilon Eridani", 1.9375, -7.75, -6.84375),
+    new Waypoint("61 Cygni", -11.21875, -1.1875, 1.40625),
+    new Waypoint("Groombridge 34", -9.90625, -3.6875, -5.09375),
+    new Waypoint("Tau Ceti", -0.375, -11.40625, -3.5),
+    new Waypoint("Luyten's Star",  6.5625, 2.34375, -10.25),
+];
 
-//var UserInput = input
-
-function QueryDatabaseForData(UserInput)
-    //Send UserInput to Database via API
-    //Request name and coordinates
-    //Place System Data into new const Waypoint
-    //GET https://www.edsm.net/api-v1/system
-    //GET https://www.edsm.net/api-v1/system?systemName=Sol&showCoordinates=1
-    //  systemName
-    //  showCoordinates
-
-function TableAddWaypoint(){
-
+// Function to add a new waypoint
+function addWaypoint(name, x, y, z) {
+    const newWaypoint = new Waypoint(name, x, y, z);
+    waypoints.push(newWaypoint);
 }
 
-function TableRemoveWaypoint(){
-
+// Function to remove a waypoint by name
+function removeWaypointByName(name) {
+    const index = waypoints.findIndex(waypoint => waypoint.name === name);
+    if (index !== -1) {
+        waypoints.splice(index, 1);
+    }
 }
 
-function TableSetWaypointToStart(){
+//addWaypoint("D", 2, 3, 0); // Add a new waypoint with name "D" and coordinates (2, 3, 0)
+//console.log("Waypoints after adding 'D':", waypoints);
 
+//removeWaypointByName("B"); // Remove waypoint with name "B"
+//console.log("Waypoints after removing 'B':", waypoints);
+
+function distance(waypoint1, waypoint2) {
+    return Math.sqrt(Math.pow(waypoint2.x - waypoint1.x, 2) + 
+                     Math.pow(waypoint2.y - waypoint1.y, 2) +
+                     Math.pow(waypoint2.z - waypoint1.z, 2));
 }
 
-function CalculateDistance(Waypoint01, Waypoint02) {
-    var a = Waypoint02.X - Waypoint01.X;
-    var b = Waypoint02.Y - Waypoint01.Y;
-    var c = Waypoint02.Z - Waypoint01.Z;
-    var result = Math.sqrt(a * a + b * b + c * c);
-    return (result.toFixed(2));
+function calculatePathDistance(path) {
+    let totalDistance = 0;
+    for (let i = 0; i < path.length - 1; i++) {
+        totalDistance += distance(path[i], path[i + 1]);
+    }
+    return totalDistance;
 }
 
-function OptimizeRoute(TableContent, TableStart){
+function permuteWaypoints(waypoints) {
+    const permutations = [];
 
+    function permute(arr, m = []) {
+        if (arr.length === 0) {
+            permutations.push(m);
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                const curr = arr.slice();
+                const next = curr.splice(i, 1);
+                permute(curr.slice(), m.concat(next));
+            }
+        }
+    }
+
+    permute(waypoints);
+    return permutations;
+}
+
+function findShortestPath(waypoints) {
+    const allPermutations = permuteWaypoints(waypoints);
+    let shortestPath = null;
+    let shortestDistance = Infinity;
+
+    allPermutations.forEach(permutation => {
+        const pathDistance = calculatePathDistance(permutation);
+        if (pathDistance < shortestDistance) {
+            shortestDistance = pathDistance;
+            shortestPath = permutation;
+        }
+    });
+
+    return shortestPath;
 }
